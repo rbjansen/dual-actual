65283585daef0050c2e21791d6d9b150a2604034
Remco Bastiaan Jansen <r.b.jansen.uu@gmail.com>
2024-11-25 00:37:33 +0300

    updates and readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # dual-actual
+
+An example docker-based solution for single-currency budgeting with multi-currency connected bank accounts in [Actual](https://actualbudget.com/).
+
+## Installation
+
+Start Actual with:
+
+```bash
+docker compose up -d --build
+```
+
+Log in, set up your accounts, and determine the ID of the account you want to apply the conversions to.
+
+> Recommended: link your bank accounts to Actual to be able to import your latest transactions with the click of a button. See Actual's instruction [here](https://actualbudget.org/docs/advanced/bank-sync/).
+
+Add a `.env` file to the root of the project with the following keys, replacing the values with your own:
+
+```bash
+ACTUAL_BUDGET_ID="<your-budget-id>"
+ACTUAL_CURRENCY="<your-currency>"  # e.g. "SEK"
+ACTUAL_CONVERT_ACCOUNT_ID="<your-account-id>"
+ACTUAL_PASSWORD="<your-password>"
+ACTUAL_PORT="<your-port>"  # e.g. 5006
+```
+
+Restart the Actual container with the new environment variables:
+
+```bash
+docker compose up -d --force-recreate
+```
+
+Note that any subsequent startups of the docker compose will automatically take the values from the `.env` file you have set.
+
+## Usage
+
+The script `entry.js` updates all transaction amounts in the specified account `ACTUAL_CONVERT_ACCOUNT_ID` that have not previously been converted, to the currency specified by `ACTUAL_CURRENCY`. The example in this repository uses exchange rates provided by the European Central Bank (ECB) via [ecb-euro-exchange-rates](https://www.npmjs.com/package/ecb-euro-exchange-rates).
+
+Manually run the script with:
+
+```bash
+docker exec -it dual-actual node entry
+```
+
+Alternatively, uncomment line 22 in `docker-compose.yml` to execute the conversion script on startup.
--- entry.js
+++ entry.js
@@ -1,32 +1,36 @@
 import api from "@actual-app/api";
 import Exchange from "./lib/exchangeRates.js";
 
-const acctId = "56d03adc-e474-48ff-968a-1e23bea963ea";
-const exchange = new Exchange();
+const exchange = new Exchange({ curreny: process.env.ACTUAL_CURRENCY });
 
 (async () => {
   await api.init({
     dataDir: "./actual-cache",
-    serverURL: "http://localhost:5006",
+    serverURL: process.env.ACTUAL_SERVER_URL || "http://localhost:5006",
     password: process.env.ACTUAL_PASSWORD,
   });
 
-  await api.downloadBudget("160f19e6-efef-4be1-9ae8-8f6f418448b9");
+  await api.downloadBudget(process.env.ACTUAL_BUDGET_ID);
   await exchange.getRates();
 
   try {
-    let transactions = await api.getTransactions(acctId);
+    let transactions = await api.getTransactions(
+      process.env.ACTUAL_CONVERT_ACCOUNT_ID
+    );
+    let count = 0;
     for (let transaction of transactions) {
-      // Skip transactions that have already been converted
-      if (transaction.notes?.startsWith("SEK: ")) {
+      // Skip transactions that have already been converted.
+      if (transaction.notes?.startsWith(`${process.env.ACTUAL_CURRENCY}: `)) {
         continue;
       }
-      console.log(exchange.applyRate(transaction.amount, transaction.date));
-      // await api.updateTransaction(transaction.id, {
-      //   notes: "SEK: " + transaction.notes,
-      //   amount: exchange.applyRate(transaction.amount, transaction.date),
-      // });
+      // NOTE: values are in cents; rounded after conversion.
+      await api.updateTransaction(transaction.id, {
+        notes: `${process.env.ACTUAL_CURRENCY}: ` + transaction.notes,
+        amount: exchange.applyRate(transaction.amount, transaction.date),
+      });
+      count++;
     }
+    console.log("Converted", count, "transactions.");
   } catch (e) {
     console.error(e);
   }
